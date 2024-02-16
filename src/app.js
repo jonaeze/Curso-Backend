a4f1978c6c4d6e02330350323db7352c0e62a7e0
jonaeze <jonathan.e.bertonasco@gmail.com>
2024-02-16 01:45:04 -0300

    Primera PreEntrega

--- src/app.js
+++ src/app.js
@@ -4,34 +4,33 @@ import ProductManager from './productManagar.class.js';
 const productManager = new ProductManager('src/products.json');
 
 const app = express();
+const PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/products' , async (request, response) => {
+app.get('/products', async (request, response) => {
     try {
         const limit = request.query.limit
         const allProducts = await productManager.getProducts();
-        console.log('Todos los productos:', allProducts);
         response.send(allProducts.slice(0, limit));
-        }
+    }
     catch (error) {
         response.send(error.message);
-    }   
-})
+    }
+});
 
-app.get('/products/:pid' , async (request, response) => {
+app.get('/products/:pid', async (request, response) => {
     try {
         const pid = parseInt(request.params.pid);
         const foundProduct = await productManager.getProductById(pid);
         response.send(foundProduct);
     }
     catch (error) {
-        console.log("error", error);
         response.send(error.message);
     }
-})
+});
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
     console.log("escuchando en el puerto 8080");
-})
+});
